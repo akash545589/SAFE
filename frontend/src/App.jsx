3195d36774d96bc36b387df00b06cd31f54b0b97
akashdeepcit <akash.deep@consiliorit.com>
2025-06-14 22:33:24 +0530

    added frontend and backend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./assets/images/image-safe-logo.png";
+import Onboarding from "./pages/Onboarding/Onboarding";
+
+function App() {
+  return (
+    <>
+      <Onboarding />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Onboarding/Onboarding.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Onboarding.css";
+function Onboarding() {
+  return (
+    <div className="container">
+      <div className="onboarding-page">
+        <div className="onboarding-left">a</div>
+        <div className="onboarding-right"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Onboarding;
